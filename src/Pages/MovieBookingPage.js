2a51ecc50aa50183a539c5fef6e12f63615f5d7e
rp520671 <rp520671@gmail.com>
2024-01-13 01:31:45 +0530

    handling changes

--- src/Pages/MovieBookingPage.js
+++ src/Pages/MovieBookingPage.js
@@ -5,26 +5,39 @@ import BookingForm from "../Components/BookingForm";
 import MovieSummary from "../Components/MovieSummary";
 
 const MovieBookingPage = () => {
-    const { state } = useLocation();
-    console.log("Movie data from useLocation", state);
-    const { movie } = state;
+  const { state } = useLocation();
+  console.log("Movie data from useLocation", state);
 
+  if (!state || !state.movie || !state.movie.show) {
     return (
-        <Container>
-            <Row>
-                <Col>
-                    <MovieSummary
-                        name={movie.show.name}
-                        summary={movie.show.summary}
-                        img={movie.show.image.original}
-                    />
-                </Col>
-                <Col className="bookingForm">
-                    <BookingForm name={movie.show.name} />
-                </Col>
-            </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col>
+            <p>No movie information available.</p>
+          </Col>
+        </Row>
+      </Container>
     );
+  }
+
+  const { movie } = state;
+
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <MovieSummary
+            name={movie.show.name}
+            summary={movie.show.summary}
+            img={movie.show.image ? movie.show.image.original : null}
+          />
+        </Col>
+        <Col className="bookingForm">
+          <BookingForm name={movie.show.name} />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default MovieBookingPage;
